d14995ea8fc8c7dacdc06ae6c53dc592effe8aad
SasukeUchiha667 <su7623748@gmail.com>
2026-04-19 15:06:06 +0500

    Add ParentPanel component with progress display

---
+++ src/components/ParentPanel.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function ParentPanel() {
+  return (
+    <div style={{margin:30}}>
+      <h2>Ата-ана панелі</h2>
+      <div>
+        <div>Балаңыздың прогресі: <b> 74% </b></div>
+        <div>Көрсеткіш графигі (имитация)</div>
+        <div>Қиын тақырыптар: <b>Қосу, Бөлшектер</b></div>
+        <div>⏱️ Оқу уақыты лимит: <b>30 мин/күніне</b></div>
+        <div>🔔 Push/Бейімделген ескерту: “Жаңа жетістік!”</div>
+      </div>
+    </div>
+  );
+}
