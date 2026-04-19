c37e15ba7b0e0bcd74b86cae91fb156bef3282b7
SasukeUchiha667 <su7623748@gmail.com>
2026-04-19 15:10:31 +0500

    Add Progress component for displaying user progress

---
+++ src/components/Progress.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+export default function Progress() {
+  return(
+    <div style={{margin:30}}>
+      <h2>Прогресс</h2>
+      <div>
+        <div>Күнделікті жаттығу: 6 / 7</div>
+        <div>Графиктер мен барлар — визуализация (имитация)</div>
+        <div>Әлсіз тақырып: <b>20%</b> өсті</div>
+      </div>
+    </div>
+  );
+}
