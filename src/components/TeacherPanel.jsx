61df102bd963a06d97f14c12f99cfe8e44385101
SasukeUchiha667 <su7623748@gmail.com>
2026-04-19 15:06:25 +0500

    Add TeacherPanel component with basic layout

---
+++ src/components/TeacherPanel.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function TeacherPanel() {
+  return (
+    <div style={{margin:30}}>
+      <h2>Мұғалім панелі</h2>
+      <div>Оқушылар саны: <b>16</b></div>
+      <div>Сабақ құру, тапсырма беру, нәтижені көру (имитация).</div>
+      <div>✉️ Кері байланыс: “Аружанның нәтижесі жақсарды!”</div>
+    </div>
+  );
+}
